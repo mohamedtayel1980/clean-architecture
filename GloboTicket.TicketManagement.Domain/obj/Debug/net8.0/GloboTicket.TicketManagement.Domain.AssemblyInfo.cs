39c00397fd975fcae99f9f9ed02fc2147de6cb4c
mohamedtayel1980 <moh_moh701@hotmail.com>
2024-07-11 03:30:42 +0300

    add Application project

--- GloboTicket.TicketManagement.Domain/obj/Debug/net8.0/GloboTicket.TicketManagement.Domain.AssemblyInfo.cs
+++ GloboTicket.TicketManagement.Domain/obj/Debug/net8.0/GloboTicket.TicketManagement.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GloboTicket.TicketManagement.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9a3d424482953472d8fb748da5bf2bd642256cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0743690b0a5db3e6eee17cbc8b156eb7df3ef9d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("GloboTicket.TicketManagement.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GloboTicket.TicketManagement.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
