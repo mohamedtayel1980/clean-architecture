fda49df19f93ae9e611945ec6a97f572867f3bcc
mohamedtayel1980 <moh_moh701@hotmail.com>
2024-07-14 17:59:00 +0300

    add mapping profile

---
+++ GloboTicket.TicketManagement.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Features.Events.Queries;
+using GloboTicket.TicketManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() {
+            CreateMap<Event, EventListVm>().ReverseMap();
+        }
+    }
+}
