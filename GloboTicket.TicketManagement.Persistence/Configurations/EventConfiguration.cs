895cbe7fa000e364a1948555e02c9ddbe5a2be4c
mohamedtayel1980 <moh_moh701@hotmail.com>
2024-07-28 14:59:05 +0300

    add persistence layer

---
+++ GloboTicket.TicketManagement.Persistence/Configurations/EventConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using GloboTicket.TicketManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GloboTicket.TicketManagement.Persistence.Configurations
+{
+    public class EventConfiguration : IEntityTypeConfiguration<Event>
+    {
+        public void Configure(EntityTypeBuilder<Event> builder)
+        {
+
+            builder.Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+
+
+        }
+    }
+}
