380dc9502fd5cf486babd866bc0e5d56cabea127
mohamedtayel1980 <moh_moh701@hotmail.com>
2024-07-28 16:41:58 +0300

    add  Repositories implemntation

---
+++ GloboTicket.TicketManagement.Persistence/Repositories/CategoryRepository.cs
@@ -0,0 +1,23 @@
+﻿using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GloboTicket.TicketManagement.Persistence.Repositories
+{
+    public class CategoryRepository : BaseRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(GloboTicketDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<List<Category>> GetCategoriesWithEvents(bool includePassedEvents)
+        {
+            var allCategories = await _dbContext.Categories.Include(x => x.Events).ToListAsync();
+            if (!includePassedEvents)
+            {
+                allCategories.ForEach(p => p.Events.ToList().RemoveAll(c => c.Date < DateTime.Today));
+            }
+            return allCategories;
+        }
+    }
+}
