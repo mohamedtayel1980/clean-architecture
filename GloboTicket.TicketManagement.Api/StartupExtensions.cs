615f2339bfba1769bd23231afaa5a5ca209c27a5
mohamedtayel1980 <moh_moh701@hotmail.com>
2024-08-04 12:40:45 +0300

    add StartupExtensions  class

---
+++ GloboTicket.TicketManagement.Api/StartupExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace GloboTicket.TicketManagement.Api
+{
+    public static class StartupExtensions
+    {
+        public static WebApplication ConfigureServices(
+           this WebApplicationBuilder builder)
+        {
+
+        }
+
+        public static WebApplication ConfigurePipeline(this WebApplication app)
+        {
+
+        }
+    }
+}
