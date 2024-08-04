8d62447aaa0b85c0b92c1c1044b674c84dabf654
mohamedtayel1980 <moh_moh701@hotmail.com>
2024-08-04 14:43:34 +0300

    add URLs for api and client

--- GloboTicket.TicketManagement.Api/StartupExtensions.cs
+++ GloboTicket.TicketManagement.Api/StartupExtensions.cs
@@ -1,11 +1,30 @@
-﻿namespace GloboTicket.TicketManagement.Api
+﻿using GloboTicket.TicketManagement.Application;
+using GloboTicket.TicketManagement.Infrastructure;
+using GloboTicket.TicketManagement.Persistence;
+namespace GloboTicket.TicketManagement.Api
 {
     public static class StartupExtensions
     {
         public static WebApplication ConfigureServices(
            this WebApplicationBuilder builder)
         {
+            builder.Services.AddApplicationServices();
+            builder.Services.AddInfrastructureServices(builder.Configuration);
+            builder.Services.AddPersistenceServices(builder.Configuration);
+            builder.Services.AddControllers();
 
+            builder.Services.AddCors(
+                options => options.AddPolicy(
+                    "open",
+                    policy => policy.WithOrigins([builder.Configuration["ApiUrl"] ?? "https://localhost:7020",
+                        builder.Configuration["BlazorUrl"] ?? "https://localhost:7080"])
+            .AllowAnyMethod()
+            .SetIsOriginAllowed(pol => true)
+            .AllowAnyHeader()
+            .AllowCredentials()));
+
+            //builder.Services.AddSwaggerGen()
+            return builder.Build();
         }
 
         public static WebApplication ConfigurePipeline(this WebApplication app)
